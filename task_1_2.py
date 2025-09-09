d07fd2265087bfd128a73a4f8d60a6275900af41
Waser Levin <ILZ1117@ilz.info>
2025-09-09 16:24:12 +0200

    Aufgaben 1-3

--- task_1_2.py
+++ task_1_2.py
@@ -1,1 +1,8 @@
-print("hier kommt aufgabe 2 hin")
+name = input("Wie lautet dein Name?")
+
+print(f"Hallo {name}! schön, dass du da bist")
+
+zahl1 = int(input("nennen sie eine zahl"))
+zahl2 = int(input("nennen sie eine zweite Zahl"))
+
+print(f"Die Summe von {zahl1} und {zahl2} beträgt {zahl1 + zahl2}")
